ae73eab2e65cf3232918b6c1942d21a52460c13c
anna <aefetter@unc.edu>
2025-01-06 20:42:09 -0500

    Update base URL for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/styles.css");
 
     return {
+        pathPrefix: "/11ty-blog-demo/", // Replace 'your-repo-name' with your actual repository name
         dir: {
             input: "src",
             output: "docs",
