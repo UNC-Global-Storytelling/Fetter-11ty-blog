191dc2ab8fdc5d2853a9df471cd0d791955ffff2
anna <aefetter@unc.edu>
2025-01-21 21:56:53 -0500

    changing the css pass through copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function (eleventyConfig) {
     // Copy `src/css/styles.css` to `_site/css/styles.css`
-    eleventyConfig.addPassthroughCopy("src/styles.css");
-
+    eleventyConfig.addPassthroughCopy("src/css/styles.css");
     return {
         dir: {
             input: "src",
